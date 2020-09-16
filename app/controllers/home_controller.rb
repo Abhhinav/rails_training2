4f302d2d4337ee50d7879f42374c72bc46332b77
abhhinav <abhinavkmr86@gmail.com>
2020-09-16 14:20:51 +0530

    pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,14 +14,14 @@ class HomeController < ApplicationController
     def tag_search
         if params.has_key? (:tag)
             result = Tag.where(name: params[:tag])
-            @posts = result.blank? ? [] : result.first.posts
+            @posts = result.blank? ? [] : result.first.posts.page(params[:page]).per(5)
         end
         render "home/index"
     end
     def category_search
         if params.has_key? (:category_slug)
             result = Category.where(slug: params[:category_slug])
-            @posts = result.blank? ? [] : result.first.posts
+            @posts = result.blank? ? [] : result.first.posts.page(params[:page]).per(5)
         end
         render "home/index"
     end
